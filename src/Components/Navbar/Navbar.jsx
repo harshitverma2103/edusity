7cdb50cf4f5b80c6ee10018e0aeb5a56fcc85454
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-16 23:12:06 +0530

    add logic to add background color after scrollng the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
-  return (
-    <nav className='container'>
-     <img className='logo' src={logo} alt="Image not found" />
-     <ul>
-          <li>Home</li>
-          <li>Program</li>
-          <li>About us</li>
-          <li>Campus</li>
-          <li>Testimonials</li>
-          <li><button className='btn'>Contact us</button></li>
-     </ul>
-    </nav>
-  )  
-}
+  const [sticky, setSticky] = useState(false);
 
-export default Navbar
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    })
+  }, []);
+  
+  return (
+    <nav className={`container ${sticky ? "dark-navbar" : ""}`}>
+      <img className="logo" src={logo} alt="Image not found" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>
+          <button className="btn">Contact us</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
