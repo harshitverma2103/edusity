a5fecfb0ddcb6855f10e86b4a53b755314e847c7
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-16 17:57:14 +0530

    create and style program section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Program from './Components/Programs/Program'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className="container">
+        <Program/>
+      </div>
     </div>
   )
 }
