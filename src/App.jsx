a78ffd08a8a85e26a2b0c8676f8837f588ea7efd
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-19 15:40:04 +0530

    create header and download npm scroll package

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
 import Testimonial from "./Components/Testimonials/Testimonial";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Testimonial/>
         <Title subTitle="CONTACT US" Title="Get in Touch"/>
         <Contact/>
+        <Footer/>
       </div>
     </div>
   );
