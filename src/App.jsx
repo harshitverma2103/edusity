05838e4469f3435ced211f1fff137b04b996ae4e
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-20 16:42:20 +0530

    add video

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Program from "./Components/Programs/Program";
@@ -8,8 +8,11 @@ import Campus from "./Components/Campus/Campus";
 import Testimonial from "./Components/Testimonials/Testimonial";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import VideoPlayer from "./Components/VideoPlayer/VideoPlayer";
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -17,15 +20,16 @@ const App = () => {
       <div className="container">
         <Title subTitle="OUR PROGRAM" Title="What We Offer" />
         <Program />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subTitle="GALLERY" Title="Campus Photos" />
         <Campus />
         <Title subTitle="TESTIMONIALS" Title="What Student Says" />
-        <Testimonial/>
-        <Title subTitle="CONTACT US" Title="Get in Touch"/>
-        <Contact/>
-        <Footer/>
+        <Testimonial />
+        <Title subTitle="CONTACT US" Title="Get in Touch" />
+        <Contact />
+        <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
